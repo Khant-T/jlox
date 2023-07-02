dc1a3146cb2d1d092eda476f1dcd4358d53da7f2
Khant Tayza <ktayza@depaul.edu>
2023-07-01 19:20:21 -0500

    inherited from Expr.Visitor

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -1,5 +1,28 @@
 package com.craftinginterpreters.lox;
 
-public class Interpreter
+public class Interpreter implements Expr.Visitor<Object>
 {
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr)
+    {
+        return null;
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr)
+    {
+        return null;
+    }
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr)
+    {
+        return null;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr)
+    {
+        return null;
+    }
 }
