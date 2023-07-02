e67069eb3eb34e333efa2be93b23b4abd1fc4440
Khant Tayza <ktayza@depaul.edu>
2023-07-02 09:09:04 -0500

    added helper stringify

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -110,4 +110,21 @@ public class Interpreter implements Expr.Visitor<Object>
         if (left instanceof Double && right instanceof Double) return;
         throw new RuntimeError(operator, "Operands must be numbers.");
     }
+
+    private String stringify(Object object)
+    {
+        if (object == null) return "nil";
+
+        if (object instanceof Double)
+        {
+            String text = object.toString();
+            if (text.endsWith(".0"))
+            {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+
+        return object.toString();
+    }
 }
