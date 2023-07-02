2f280ae397d29a52cd8edc4fa0dd700db4a222fc
Khant Tayza <ktayza@depaul.edu>
2023-07-02 09:24:45 -0500

    THE INTERPRETER IS ALIVE NOW!

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 public class Lox
 {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
     static boolean hadRuntimeError = false;
 
@@ -80,6 +81,7 @@ public class Lox
         if (hadError) return;
 
         System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message)
