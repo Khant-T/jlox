23cae4f034ddbda24fbcc5358a310a60b824451a
Khant Tayza <ktayza@depaul.edu>
2023-07-04 16:30:51 -0500

    added support for Stmt classes

--- src/com/craftinginterpreters/lox/AstPrinter.java
+++ src/com/craftinginterpreters/lox/AstPrinter.java
@@ -1,10 +1,43 @@
 package com.craftinginterpreters.lox;
 
-public class AstPrinter implements Expr.Visitor<String>
+public class AstPrinter implements Expr.Visitor<String>, Stmt.Visitor<String>
 {
     String print(Expr expr)
     {
         return expr.accept(this);
+    }
+
+    String print(Stmt stmt)
+    {
+        return stmt.accept(this);
+    }
+
+    @Override
+    public String visitPrintStmt(Stmt.Print stmt)
+    {
+        return parenthesize("print");
+    }
+
+    @Override
+    public String visitExpressionStmt(Stmt.Expression stmt)
+    {
+        if (stmt.expression instanceof Expr.Binary)
+            return visitBinaryExpr((Expr.Binary)stmt.expression);
+        if (stmt.expression instanceof Expr.Unary)
+            return visitUnaryExpr((Expr.Unary)stmt.expression);
+        if (stmt.expression instanceof Expr.Grouping)
+            return visitGroupingExpr((Expr.Grouping)stmt.expression);
+        if (stmt.expression instanceof Expr.Literal)
+            return visitLiteralExpr((Expr.Literal)stmt.expression);
+
+        // Unreachable.
+        return null;
+    }
+
+    @Override
+    public String visitVarStmt(Stmt.Var stmt)
+    {
+        return "(define " + stmt.name.lexeme + ")";
     }
 
     @Override
@@ -30,6 +63,12 @@ public class AstPrinter implements Expr.Visitor<String>
     public String visitUnaryExpr(Expr.Unary expr)
     {
         return parenthesize(expr.operator.lexeme, expr.right);
+    }
+
+    @Override
+    public String visitVariableExpr(Expr.Variable expr)
+    {
+        return "define " + expr.name;
     }
 
     private String parenthesize(String name, Expr... exprs)
