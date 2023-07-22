d97d9d6ca9f53172d1e61b142c55489c39e68efd
Khant Tayza <ktayza@depaul.edu>
2023-07-22 17:12:03 -0500

    added class declaration

--- src/com/craftinginterpreters/lox/Resolver.java
+++ src/com/craftinginterpreters/lox/Resolver.java
@@ -37,6 +37,14 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>
         return null;
     }
 
+    @Override
+    public Void visitClassStmt(Stmt.Class stmt)
+    {
+        declare(stmt.name);
+        define(stmt.name);
+        return null;
+    }
+
     public Void visitExpressionStmt(Stmt.Expression stmt)
     {
         resolve(stmt.expression);
